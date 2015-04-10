db3fb5883a5b86d467e626199a3a5cd006966642
SAENE <SAENE@192.168.1.61>
2015-04-09 22:39:00 -0300

    Atualização do construtor.

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+
+    public SaldoInsuficienteException(String não_é_possivel_efetuar_a_compra_saldo_ins) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
